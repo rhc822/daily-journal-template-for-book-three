d4d5624214ba6ef8510b3b859f9ff8e641a6f890
rhc822 <rhc822@gmail.com>
2019-10-30 13:26:56 -0500

    I'm seeing the data that is entered into the form fields.

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -8,5 +8,19 @@ import entryComponent from "./entryComponent.js"
     Change the fake variable names below to what they should be
     to get the data and display it.
 */
-API.getJournalEntries()
-.then(response => renderDOM.renderJournalEntries(response))
+
+const dateInput = document.querySelector("#dateInput").value;
+const conceptInput = document.querySelector("#conceptInput").value;
+const entryInput = document.querySelector("#entryInput").value;
+const moodInput = document.querySelector("#moodInput").value;
+
+document.querySelector("button").addEventListener("click", () => { //Watches the button
+    const createJournalEntry = function(date, concept, entry, mood) {
+        return {
+            "date": "`${createJournalEntry.date}"
+        }
+    debugger
+})
+
+// API.getJournalEntries()
+// .then(response => renderDOM.renderJournalEntries(response))
