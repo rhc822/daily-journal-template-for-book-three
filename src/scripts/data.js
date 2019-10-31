144ec42e1b4a3b054246dd971124500d5e4c9bcd
rhc822 <rhc822@gmail.com>
2019-10-31 10:56:20 -0500

    completed journal #7

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,8 +1,29 @@
-const API = {
-  getJournalEntries () {
-      return fetch("http://localhost:3000/entries")
-          .then(response => response.json())
+/* 
+
+The API variable has two objects that are functions (methods to that variable). 
+
+"getJournalEntries" gets the entire list in the database (API).
+
+"sendNewJournalEntry" adds the new journal entry to the database (API) and JSON-ifies it.
+
+*/
+
+const API =
+  {
+        getJournalEntries () {
+            return fetch("http://localhost:3000/entries")
+                .then(response => response.json())
+        }, 
+
+      sendNewJournalEntry (newJournalEntry) {
+        return fetch("http://localhost:3000/entries", { // Replace "url" with your API's URL
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+        },
+            body: JSON.stringify(newJournalEntry)
+      })
+    }
   }
-}
 
 export default API;
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -9,18 +9,27 @@ import entryComponent from "./entryComponent.js"
     to get the data and display it.
 */
 
-const dateInput = document.querySelector("#dateInput").value;
-const conceptInput = document.querySelector("#conceptInput").value;
-const entryInput = document.querySelector("#entryInput").value;
-const moodInput = document.querySelector("#moodInput").value;
 
-document.querySelector("button").addEventListener("click", () => { //Watches the button
-    const createJournalEntry = function(date, concept, entry, mood) {
-        return {
-            "date": "`${createJournalEntry.date}"
-        }
-    debugger
+/* 
+
+This code watches for when the "Record Journal Entry" button is pressed. Then, it looks at the values that were entered into the fields above and puts them in "newJournalEntry" as an object. Next, the new journal entry is passed to the sendNewJournalEntry function in data.js file. It returns JSON-ified entry, gets all the journal entries, and adds them to the DOM.
+
+*/
+
+document.querySelector("button").addEventListener("click", () => {
+    const dateInput = document.querySelector("#dateInput").value;
+    const conceptInput = document.querySelector("#conceptInput").value;
+    const entryInput = document.querySelector("#entryInput").value;
+    const moodInput = document.querySelector("#moodInput").value;
+    const newJournalEntry = { //Puts the above values into "newJournalEntry" as an object
+        "date": dateInput,
+        "concept": conceptInput,
+        "entry": entryInput,
+        "mood": moodInput
+    }
+    API.sendNewJournalEntry(newJournalEntry) //runs function that post new entry
+    .then(API.getJournalEntries).then(response => renderDOM.renderJournalEntries(response)) //Then gets all the journal entries, then puts 'em on the DOM
 })
 
-// API.getJournalEntries()
-// .then(response => renderDOM.renderJournalEntries(response))
+//Provides the initial display of data to the DOM
+API.getJournalEntries().then(response => renderDOM.renderJournalEntries(response))
